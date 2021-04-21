ade616d50afe4d9d6bc9408bce8c24ed5ae18d13
Sasha <al.kanenckoff@yandex.ru>
2021-04-21 22:13:57 +0300

    version 0.2.1 21.04.2021

---
+++ CourseWork/src/main/java/com/example/demo/Repositories/TruckRepository.java
@@ -0,0 +1,22 @@
+package com.example.demo.Repositories;
+
+import com.example.demo.Model.Truck;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * jpa репозиторий для грузовиков
+ *
+ * @author kanenkovaa
+ * @version 0.2
+ */
+public interface TruckRepository extends CrudRepository<Truck, Long> {
+
+    /**
+     * Поиск грузовиков по характеристике
+     * @param description - характеристика грузовика
+     * @return description - характеристика грузовика
+     */
+    List<Truck> findAllByDescription(String description);
+}
---
+++ CourseWork/src/main/java/com/example/demo/Services/TruckService.java
@@ -0,0 +1,31 @@
+package com.example.demo.Services;
+
+import com.example.demo.Model.Truck;
+import com.example.demo.Repositories.TruckRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Сервис, осуществляющий работу с таблицей грузовиков
+ *
+ * @author kanenkovaa
+ * @version 0.2
+ */
+@Service
+public class TruckService {
+
+    @Autowired
+    private TruckRepository truckRepository;
+
+    /**
+     * Поиск грузовиков по характеристике
+     *
+     * @param description - характеристика грузовика
+     * @return description - характеристика грузовика
+     */
+    public List<Truck> findAllByDescription(String description) {
+        return truckRepository.findAllByDescription(description);
+    }
+}
